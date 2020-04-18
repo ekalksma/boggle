8bfc167f8208e79fcc091c6ed681c18432b895a1
Emiel Kalksma <emiel.kalksma@gmail.com>
2020-04-18 16:17:04 +0200

    Create boggle class

--- src/index.js
+++ src/index.js
@@ -1,60 +1,66 @@
 import $ from 'jquery';
 
-const dice = [
-  ['R', 'I', 'F', 'O', 'B', 'X'],
-  ['I', 'F', 'E', 'H', 'E', 'Y'],
-  ['D', 'E', 'N', 'O', 'W', 'S'],
-  ['U', 'T', 'O', 'K', 'N', 'D'],
-  ['H', 'M', 'S', 'R', 'A', 'O'],
-  ['L', 'U', 'P', 'E', 'T', 'S'],
-  ['A', 'C', 'I', 'T', 'O', 'A'],
-  ['Y', 'L', 'G', 'K', 'U', 'E'],
-  ['Q', 'B', 'M', 'J', 'O', 'A'],
-  ['E', 'H', 'I', 'S', 'P', 'N'],
-  ['V', 'E', 'T', 'I', 'G', 'N'],
-  ['B', 'A', 'L', 'I', 'Y', 'T'],
-  ['E', 'Z', 'A', 'V', 'N', 'D'],
-  ['R', 'A', 'L', 'E', 'S', 'C'],
-  ['U', 'W', 'I', 'L', 'R', 'G'],
-  ['P', 'A', 'C', 'E', 'M', 'D']
-];
+class Boggle {
+  constructor() {
+    const dice = [
+      ['R', 'I', 'F', 'O', 'B', 'X'],
+      ['I', 'F', 'E', 'H', 'E', 'Y'],
+      ['D', 'E', 'N', 'O', 'W', 'S'],
+      ['U', 'T', 'O', 'K', 'N', 'D'],
+      ['H', 'M', 'S', 'R', 'A', 'O'],
+      ['L', 'U', 'P', 'E', 'T', 'S'],
+      ['A', 'C', 'I', 'T', 'O', 'A'],
+      ['Y', 'L', 'G', 'K', 'U', 'E'],
+      ['Q', 'B', 'M', 'J', 'O', 'A'],
+      ['E', 'H', 'I', 'S', 'P', 'N'],
+      ['V', 'E', 'T', 'I', 'G', 'N'],
+      ['B', 'A', 'L', 'I', 'Y', 'T'],
+      ['E', 'Z', 'A', 'V', 'N', 'D'],
+      ['R', 'A', 'L', 'E', 'S', 'C'],
+      ['U', 'W', 'I', 'L', 'R', 'G'],
+      ['P', 'A', 'C', 'E', 'M', 'D']
+    ];
 
-function getRandomBoard(dice) {
-  const board = [];
+    this.word = [];
+    this.isMouseDown = false;
+    const board = this.getRandomBoard(dice);
 
-  for (const die of dice) {
-    const randomDieValue = die[Math.floor(Math.random() * Math.floor(6))];
-    board.push(randomDieValue);
+    this.drawBoard(board);
+
+    $('#board .letter').mousedown((event) => {
+      this.isMouseDown = true;
+      this.word.push($(event.target).text());
+    });
+
+    $('body').mouseup(() => {
+      this.isMouseDown = false;
+      console.log(this.word);
+      this.word = [];
+    });
+
+    $('#board .letter').mouseenter((event) => {
+      if (this.isMouseDown) {
+        this.word.push($(event.target).text());
+      }
+    });
   }
 
-  return board;
-}
+  getRandomBoard(dice) {
+    const board = [];
 
-function drawBoard(board) {
-  $('#board .letter').each(function (index) {
-    $(this).text(board[index]);
-  });
-}
+    for (const die of dice) {
+      const randomDieValue = die[Math.floor(Math.random() * Math.floor(6))];
+      board.push(randomDieValue);
+    }
 
-const board = getRandomBoard(dice);
-drawBoard(board);
-
-let word = [];
-let isMouseDown = false;
-
-$('#board .letter').mousedown(function () {
-  isMouseDown = true;
-  word.push($(this).text());
-});
-
-$('body').mouseup(function () {
-  isMouseDown = false;
-  console.log(word);
-  word = [];
-});
-
-$('#board .letter').mouseenter(function () {
-  if (isMouseDown) {
-    word.push($(this).text());
+    return board;
   }
-});
+
+  drawBoard(board) {
+    $('#board .letter').each(function (index) {
+      $(this).text(board[index]);
+    });
+  }
+}
+
+const boggle = new Boggle();
