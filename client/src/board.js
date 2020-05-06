461714dc6f83e9f775fe25cd3519025983b2ee50
Emiel Kalksma <emiel.kalksma@gmail.com>
2020-05-06 16:51:35 +0200

    Basic react setup

---
+++ client/src/board.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Square from './square';
+
+export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
+  renderSquare(i) {
+    return (<Square
+            value = {this.state.squares[i]}
+            onClick = {() => this.handleClick(i)}
+            />
+          );
+    }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ client/src/game.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Board from './board';
+
+export default class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
